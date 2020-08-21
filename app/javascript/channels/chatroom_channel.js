9432cf76f6c8bf267583a9c6bf4fc1933f85de9c
hank <hank8451@gmail.com>
2020-08-21 20:35:58 +0800

    刪掉註解

--- app/javascript/channels/chatroom_channel.js
+++ app/javascript/channels/chatroom_channel.js
@@ -11,28 +11,18 @@ document.addEventListener('turbolinks:load', () => {
   // console.log(channelId);
   consumer.subscriptions.create({channel: "ChatroomChannel", chatroom_id: channelId}, {
     connected() {
-      // Called when the subscription is ready for use on the server
       console.log(`Web Socket server connecting! ${channelId}`)
-      // console.log(this.received)
     },
   
     disconnected() {
-      // Called when the subscription has been terminated by the server
     },
   
     received(data) {
-      // Called when there's incoming data on the websocket for this channel
-      // console.log(data.message)
       const msgBox = document.querySelector("#messagelist")
       const newMsg = document.createElement("li")
       newMsg.innerHTML = data.message
-      // console.log(newMsg)
       msgBox.insertAdjacentElement("afterbegin", newMsg)
       document.forms[0].reset()
-      // clearup()
-      // const submit = document.querySelector("input[type='submit']")
-      
-      // console.log(document.forms[0])
     }
   });
 
